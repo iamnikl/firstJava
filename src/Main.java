5f4183c19e96ee8f1e6a2fd2897a4c3e4c5b4350
iamnikl <niklas.puetz@gmail.com>
2023-03-23 22:18:50 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 import javax.swing.*;
 
-
-class main {
+class Window {
+    static String[] keywords = {"Hallo", "Nikl"};
     public static void main(String[] args) {
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(100, 150);
+        frame.setSize(400, 300);
         frame.setLocation(100,100);
         frame.setDefaultLookAndFeelDecorated(true);
 
+        JLabel header = new JLabel("Hallo, mein Name ist " + keywords[0]);
+        header.setBounds(50, 50, 200, 50);
+
+        frame.add(header);
         frame.setVisible(true);
+        second();
+    }
+    public static void getProfile() {
+        String name = "Nikl"
     }
 }
