b5dbe79bdc1a2a50758acec94f5065cf143e5645
iamnikl <niklas.puetz@gmail.com>
2023-03-23 20:34:55 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.Scanner;
+import javax.swing.*;
+
+
+class main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(100, 150);
+        frame.setLocation(100,100);
+        frame.setDefaultLookAndFeelDecorated(true);
+
+        frame.setVisible(true);
     }
 }
